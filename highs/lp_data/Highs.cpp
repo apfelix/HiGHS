36ffdc983cdc0387ac4a82b6576afe1590668a83
Felix Hennings <felix@doingthemath.nl>
2025-08-28 12:57:32 +0200

    print status

--- highs/lp_data/Highs.cpp
+++ highs/lp_data/Highs.cpp
@@ -3801,7 +3801,8 @@ HighsStatus Highs::completeSolutionFromDiscreteAssignment() {
     basis_.clear();
     // FELIX addition
     // write model to mps just before calling optimize
-    HighsStatus dummy_status = writeLocalModel(model_, "highs_start_solution_debugging.mps");
+    HighsStatus felix_status = writeLocalModel(model_, "highs_start_solution_debugging.mps");
+    printf("################################################# FELIX TEST HERE %d\n", felix_status);
     return_status = this->optimizeModel();
     // ... remembering to recover the original value of mip_max_nodes
     options_.mip_max_nodes = mip_max_nodes;
