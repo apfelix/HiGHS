bf68ab1d61d2b6dcb115417d470ba7bd96dcd48a
Felix Hennings <felix@doingthemath.nl>
2025-08-28 13:10:46 +0200

    replace printf by highsLogUser

--- highs/lp_data/Highs.cpp
+++ highs/lp_data/Highs.cpp
@@ -3802,7 +3802,7 @@ HighsStatus Highs::completeSolutionFromDiscreteAssignment() {
     // FELIX addition
     // write model to mps just before calling optimize
     HighsStatus felix_status = writeLocalModel(model_, "highs_start_solution_debugging.mps");
-    printf("################################################# FELIX TEST HERE %d\n", felix_status);
+    highsLogUser(options_.log_options, HighsLogType::kInfo,"################################################# FELIX TEST HERE %d\n", int(felix_status));
     return_status = this->optimizeModel();
     // ... remembering to recover the original value of mip_max_nodes
     options_.mip_max_nodes = mip_max_nodes;
